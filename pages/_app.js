a3fd137db0c28a2216f5d112b83917dd7e7803a3
jadamconnor <j.adam.connor@gmail.com>
2022-01-27 12:29:14 -0600

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -12,8 +12,51 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+    
+    useEffect(() => {
+        
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+        
+    }, [router.events])
+    
+    return (
+        <>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gtag.GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+        )
+    }
+    
+    export default MyApp
